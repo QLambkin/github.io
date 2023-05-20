5cab15bc8adaa1912622d97e3e715e1bb58bd40d
Quentin <q.lambkin@gmail.com>
2023-05-20 11:35:21 -0400

    Set text size to vmin on home page

--- src/components/SkillsBall/SkillsBall.js
+++ src/components/SkillsBall/SkillsBall.js
@@ -3,43 +3,43 @@ import TagCloud from 'TagCloud'
 import './SkillsBall.scss'
 
 const Tags = [
-    'HTML',
-    'CSS',
-    'JAVASCRIPT',
-    'MONGOOSE',
-    'REACT',
-    'C#',
-    'PYTHON',
-    'SASS',
-    'NODE.JS',
-    'MONGODB',
-    'BOOTSTRAP',
-    'C',
-    'EXPRESS',
-    'MYSQL',
-    'JQUERY',
-    'JAVA',
-    'C++'
+  'HTML',
+  'CSS',
+  'JAVASCRIPT',
+  'MONGOOSE',
+  'REACT',
+  'C#',
+  'PYTHON',
+  'SASS',
+  'NODE.JS',
+  'MONGODB',
+  'BOOTSTRAP',
+  'C',
+  'EXPRESS',
+  'MYSQL',
+  'JQUERY',
+  'JAVA',
+  'C++',
 ]
 
 function SkillsBall() {
-    const IsTagCloudLoaded = useRef(false)
+  const IsTagCloudLoaded = useRef(false)
 
-    useEffect(() => {
-        if (IsTagCloudLoaded.current) return
+  useEffect(() => {
+    if (IsTagCloudLoaded.current) return
 
-        TagCloud('.content', Tags, {
-            radius: 400,
-            maxSpeed: 'fast',
-            initSpeed: 'fast',
-            direction: 135,
-            keep: true,
-        }) 
- 
-        IsTagCloudLoaded.current = true 
-    }, [])
- 
-    return (<div className='content' />)
+    TagCloud('.content', Tags, {
+      radius: 400,
+      maxSpeed: 'fast',
+      initSpeed: 'fast',
+      direction: 135,
+      keep: true,
+    })
+
+    IsTagCloudLoaded.current = true
+  }, [])
+
+  return <div className="content" />
 }
-  
-export default SkillsBall;  
+
+export default SkillsBall
